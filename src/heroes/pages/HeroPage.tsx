c5b73cb1721281c121fe7e64f0c8629d155dd378
Lennin Ibarra <ing.lenninibarra@gmail.com>
2025-06-04 17:29:38 -0500

    feature/mostrando informacion en HeroPage y boton para volver a la pagina anterior

--- src/heroes/pages/HeroPage.tsx
+++ src/heroes/pages/HeroPage.tsx
@@ -1,16 +1,57 @@
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import { getHeroById } from '../helpers';
+import { useMemo } from 'react';
+import { Hero } from '../interfaces/Hero';
 
 export const HeroPage = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
 
-  const hero = getHeroById(id ?? '');
+  const hero: Hero | undefined = useMemo(() => getHeroById(id ?? ''), [id]);
+
+  const onNavigateBack = () => {
+    navigate(-1);
+  };
 
   if (!hero) {
     return <Navigate to="/marvel" />;
   }
 
-  console.log(hero);
+  const { superhero, alter_ego, characters, first_appearance, publisher } =
+    hero;
 
-  return <h1>fdgfd</h1>;
+  return (
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          className="img-thumbnail"
+          src={`/assets/heroes/${id}.jpg`}
+          alt={superhero}
+        />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego</b>
+            {alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher</b>
+            {publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appearance</b>
+            {first_appearance}
+          </li>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{characters}</p>
+
+        <button className="btn btn-outline-primary" onClick={onNavigateBack}>
+          Regresar
+        </button>
+      </div>
+    </div>
+  );
 };
