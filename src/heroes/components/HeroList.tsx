63e69ede5adfcae8a39474f58e133938418a536e
Lennin Ibarra <ing.lenninibarra@gmail.com>
2025-06-05 10:29:00 -0500

    refactor/implementando useMemo en funciones

--- src/heroes/components/HeroList.tsx
+++ src/heroes/components/HeroList.tsx
@@ -2,13 +2,17 @@ import { Hero } from '../interfaces/Hero';
 import { HeroCard } from './HeroCard';
 import { getHeroesByPublisher } from '../helpers';
 import { Publisher } from '../types/Publisher';
+import { useMemo } from 'react';
 
 interface HeroListProps {
   publisher: Publisher;
 }
 
 export const HeroList: React.FC<HeroListProps> = ({ publisher }) => {
-  const heroes: Hero[] = getHeroesByPublisher(publisher);
+  const heroes: Hero[] = useMemo(
+    () => getHeroesByPublisher(publisher),
+    [publisher],
+  );
 
   return (
     <div className="row rows-cols-1 row-cols-md-3 g-3">
