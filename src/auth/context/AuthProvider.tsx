7f30e974cb5c3c86c0b59a382dc7a4c788e0a655
Lennin Ibarra <ing.lenninibarra@gmail.com>
2025-06-07 11:19:26 -0500

    feature/mantener el usuario activo, funcion de inicializacion

--- src/auth/context/AuthProvider.tsx
+++ src/auth/context/AuthProvider.tsx
@@ -2,23 +2,46 @@ import { AuthContext } from './AuthContext';
 import { ReactNode, useReducer } from 'react';
 import { authReducer } from './authReducer';
 import { types } from '../types/types';
-import { AuthState } from '../interfaces/AuthState';
+// import { AuthState } from '../interfaces/AuthState';
 
-const initialState: AuthState = {
-  logged: false,
+// const initialState: AuthState = {
+//   logged: false,
+// };
+
+//funcion de inicialización
+const init = () => {
+  const userString = localStorage.getItem('user');
+  const user = userString ? JSON.parse(userString) : null;
+
+  if (user) {
+    return {
+      logged: true,
+      user: user,
+    };
+  } else {
+    return {
+      logged: false,
+      user: null,
+    };
+  }
 };
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [authState, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, {}, init);
 
   const login = (name = '') => {
+    const user = {
+      id: 'ABC',
+      name: name,
+    };
+
     const action = {
       type: types.login,
-      payload: {
-        id: 'ABC',
-        name: name,
-      },
+      payload: user,
     };
+
+    localStorage.setItem('user', JSON.stringify(user));
+
     dispatch(action);
   };
 
